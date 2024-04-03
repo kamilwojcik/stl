df59ed347b38df070c240c102446793c3b669609
Kamil Wojcik <kamil.fiz@gmail.com>
2024-04-03 10:32:23 +0200

    Distance and average implemented

---
+++ homework/inner-product/arithmeticAverage.hpp
@@ -0,0 +1,9 @@
+#ifndef ARITHMETICAVERAGE
+#define ARITHMETICAVERAGE
+
+#include <vector>
+
+double ArithmeticAverage(std::vector<double> v1, std::vector<double> v2);
+double Distance(std::vector<double> point1, std::vector<double> point2);
+
+#endif
